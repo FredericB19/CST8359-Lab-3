d2591ad6e04ecd888d4cef258a57b2e773f27bd8
FredericB19 <55847205+FredericB19@users.noreply.github.com>
2021-06-12 09:45:30 -0400

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Lab3.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Razor()
+        {
+            return View();
+        }
+
+        public IActionResult Count()
+        {
+            HttpContext.Session.SetString("bottles", Request.Form["bottles"]);
+
+            return View();
+        }
+
+        public IActionResult CreatePerson()
+        {
+            return View();
+        }
+
+        public IActionResult DisplayPerson()
+        {
+            HttpContext.Session.SetString("firstName", Request.Form["firstName"]);
+            HttpContext.Session.SetString("lastName", Request.Form["lastName"]);
+            HttpContext.Session.SetString("age", Request.Form["age"]);
+            HttpContext.Session.SetString("emailAddress", Request.Form["emailAddress"]);
+            HttpContext.Session.SetString("dateOfBirth", Request.Form["dateOfBirth"]);
+            HttpContext.Session.SetString("description", Request.Form["description"]);
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab3.Models
+{
+    public class Person
+    {
+        [Required]
+        public string FirstName
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public string LastName
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public int Age
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public string EmailAddress
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public string Birthday
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public string Password
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public string Description
+        {
+            get;
+            set;
+        }
+    }
+}
